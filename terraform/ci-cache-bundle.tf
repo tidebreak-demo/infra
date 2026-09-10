a1264ce035e0b57977563990897671d916eb6beb
Dan Whitfield <dan.whitfield@atomicmail.io>
2026-09-10 23:51:42 +0300

    Cache the bundle between ci runs (ci-cache-bundle.tf)

--- terraform/ci-cache-bundle.tf
+++ terraform/ci-cache-bundle.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "ci_cache_bundle" {
   name                       = "ci-cache-bundle"
-  visibility_timeout_seconds = 180
+  visibility_timeout_seconds = 120
   message_retention_seconds  = 345600
 }
