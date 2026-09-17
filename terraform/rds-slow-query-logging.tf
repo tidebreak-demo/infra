3971f02f9cac1f81df3a996e93a4d90ddd366ec2
Maya Ortiz <maya.ortiz@atomicmail.io>
2026-09-18 00:21:57 +0300

    Turn on slow query logging (rds-slow-query-logging.tf)

---
+++ terraform/rds-slow-query-logging.tf
@@ -0,0 +1,39 @@
+resource "aws_sqs_queue" "rds_slow_query_logging_dlq" {
+  name                      = "rds-slow-query-logging-dlq"
+  message_retention_seconds = 1209600
+
+  tags = {
+    Service = "dispatch"
+    Queue   = "rds-slow-query-logging"
+  }
+}
+
+resource "aws_sqs_queue" "rds_slow_query_logging" {
+  name                       = "rds-slow-query-logging"
+  visibility_timeout_seconds = 120
+  message_retention_seconds  = 345600
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.rds_slow_query_logging_dlq.arn
+    maxReceiveCount     = 5
+  })
+
+  tags = {
+    Service = "dispatch"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_slow_query_logging_backlog" {
+  alarm_name          = "rds-slow-query-logging-backlog"
+  namespace           = "AWS/SQS"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  statistic           = "Maximum"
+  period              = 60
+  evaluation_periods  = 5
+  threshold           = 50
+  comparison_operator = "GreaterThanThreshold"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.rds_slow_query_logging.name
+  }
+}
